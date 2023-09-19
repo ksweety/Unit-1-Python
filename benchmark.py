187ad7f9a7760d76d59ee36594de73afe39d7094
ksweety <kahlilw03@gmail.com>
2023-09-19 09:50:57 -0400

    Exercising With Python

---
+++ benchmark.py
@@ -0,0 +1,40 @@
+# Exercise 1: 
+print("Kahlil Williams") 
+
+# Exercise 2: 
+
+favoriteshow= "Family Guy"  
+
+#Exercise 3: 
+
+age=16
+height=176.5 
+print(age * height)   
+
+#Exercise 4: 
+
+print("im going to break")  
+
+#Exercise 5: 
+
+fav_food=["pizza", "burrito", "sandwich"]
+
+#Exercise 6: 
+if age >=21:
+    print("You can rent a car") 
+else:
+    print("you are beat")
+
+# Exercise 7: 
+
+name=input('Please enter your name') 
+print("Hello" +name+"")  
+
+# Exercise 8 
+for i in fav_food:
+    print(i)
+
+# Exercise 9 
+while age>71:
+    print("oh yea we gettin money")
+    
